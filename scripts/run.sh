9d1a4569f523ddb594eb8f50b09d578cd60e24fb
fuzzy-mcfuzzface <116034335+fuzzy-mcfuzzface@users.noreply.github.com>
2022-10-17 22:12:19 +0200

    Do not run litestream at the moment

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-# set -e
+set -e
 
-# Restore the database if it does not already exist.
-if [ -f /pb_data/data.db ]; then
-	echo "Database already exists, skipping restore"
-else
-	echo "No database found, restoring from replica if exists"
-	litestream restore -v -if-replica-exists -o /pb_data/data.db "${REPLICA_URL}"
-fi
-
+# # Restore the database if it does not already exist.
+# if [ -f /pb_data/data.db ]; then
+# 	echo "Database already exists, skipping restore"
+# else
+# 	echo "No database found, restoring from replica if exists"
+# 	litestream restore -v -if-replica-exists -o /pb_data/data.db "${REPLICA_URL}"
+# fi
+# 
 # Run litestream with your app as the subprocess.
-exec litestream replicate -exec "/usr/local/bin/pocketbase --dir /pb_data serve --http 0.0.0.0:8090"
+# exec litestream replicate -exec "/usr/local/bin/pocketbase --dir /pb_data serve --http 0.0.0.0:8090"
+
+# just run pocketbase for now
+/usr/local/bin/pocketbase --dir /pb_data serve --http 0.0.0.0:8090
